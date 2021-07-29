5e5f79aa4d6f6279378479b92af53b280920d2fe
komal popli <komalpopli100@gmail.com>
2021-07-29 08:29:55 +0530

    First commit

--- csrserver/csrserver/src/main/java/com/csr/repo/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.csr.repo;
-
-import com.csr.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
